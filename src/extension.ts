e7314d94926a348a452e9722a1f037c4600e0a30
Stabbyiscool <elocreatorelo@gmail.com>
2025-04-26 00:12:21 -0500

    auto: update src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import { registerGitSaver } from './gitsaver';
 export function activate(context: vscode.ExtensionContext) {
   registerSha256(context);
   registerGitSaver();
+  registerGitPushAll();
 }
 
 export function deactivate() {}
